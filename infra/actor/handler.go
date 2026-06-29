298146eeff480d8e199cb5c99ba63497ed3217e6
godyy <188611208@qq.com>
2026-06-29 14:51:06 +0800

    更新HandlerMap

--- infra/actor/handler.go
+++ infra/actor/handler.go
@@ -1,15 +1,18 @@
 package actor
 
-import "github.com/godyy/gactor"
+import (
+	"github.com/godyy/gactor"
+	"github.com/godyy/ggskit/base/protocol"
+)
 
 // HandlerFunc
 type HandlerFunc = gactor.HandlerFunc
 
 // HandlerMap 处理函数映射.
-type HandlerMap map[uint16]HandlerFunc
+type HandlerMap map[protocol.PID]HandlerFunc
 
 // Register 注册处理函数.
-func (hm HandlerMap) Register(pid uint16, funcs ...HandlerFunc) bool {
+func (hm HandlerMap) Register(pid protocol.PID, funcs ...HandlerFunc) bool {
 	if _, ok := hm[pid]; ok {
 		return false
 	}
